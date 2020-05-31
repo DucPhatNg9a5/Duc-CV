32c85dcaab2f5e584c9d7dc186189b94114b4208
phat nguyen <you@example.com>
2020-05-30 20:31:38 -0600

    Updates

---
+++ precache-manifest.65a75a0a7acc563213f9cda743ee1cab.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67327918a5f100faf4827a82c253389a",
+    "url": "/Duc-CV/index.html"
+  },
+  {
+    "revision": "e93f7fbd82720c68f18f",
+    "url": "/Duc-CV/static/css/main.9c81d291.chunk.css"
+  },
+  {
+    "revision": "e559da2777f28bd48d49",
+    "url": "/Duc-CV/static/js/2.2f7028d7.chunk.js"
+  },
+  {
+    "revision": "c2b22595fbaa760a114537ee731d92a3",
+    "url": "/Duc-CV/static/js/2.2f7028d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e93f7fbd82720c68f18f",
+    "url": "/Duc-CV/static/js/main.fb55bb06.chunk.js"
+  },
+  {
+    "revision": "75fdde4460e93ccd979c",
+    "url": "/Duc-CV/static/js/runtime-main.875e8b66.js"
+  },
+  {
+    "revision": "9a30658c9e013cfe34fe10d6ad4cd100",
+    "url": "/Duc-CV/static/media/cisco_ccna_R_26S.9a30658c.png"
+  },
+  {
+    "revision": "9473b09691d8fa1743af227fe5442388",
+    "url": "/Duc-CV/static/media/logo200px.9473b096.png"
+  },
+  {
+    "revision": "b7b8d45aac1ce4269f22c43353de6f0f",
+    "url": "/Duc-CV/static/media/mountain.b7b8d45a.png"
+  },
+  {
+    "revision": "b64c77e4bd5ac20961a3b28bbf1669e1",
+    "url": "/Duc-CV/static/media/name.b64c77e4.png"
+  },
+  {
+    "revision": "5902a1d8b40d0a35b58b7c0e52a3f612",
+    "url": "/Duc-CV/static/media/portrait.5902a1d8.png"
+  },
+  {
+    "revision": "3c15598ef5bd08c3072b7181c425f482",
+    "url": "/Duc-CV/static/media/server.3c15598e.png"
+  },
+  {
+    "revision": "c4a7f5e10c478d0fe56714759154e4bf",
+    "url": "/Duc-CV/static/media/sun.c4a7f5e1.png"
+  }
+]);
